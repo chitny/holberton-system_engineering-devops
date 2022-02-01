336bd96ba4e346c05cc98f98042faf59a177b8f1
Ignacio Chitnisky <elinmortal@gmail.com>
2022-02-01 01:14:28 +0000

    fixing all indent

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -15,15 +15,15 @@ todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 todos = todos.json()
 todosAll = {}
 
-   for user in users:
-        taskList = []
-        for task in todos:
-            if task.get('userId') == user.get('id'):
-                taskDict = {"username": user.get('username'),
-                            "task": task.get('title'),
-                            "completed": task.get('completed')}
-                taskList.append(taskDict)
-        todosAll[user.get('id')] = taskList
+for user in users:
+    taskList = []
+    for task in todos:
+        if task.get('userId') == user.get('id'):
+            taskDict = {"username": user.get('username'),
+                        "task": task.get('title'),
+                        "completed": task.get('completed')}
+            taskList.append(taskDict)
+    todosAll[user.get('id')] = taskList
 
-    with open('todo_all_employees.json', mode='w') as f:
-        json.dump(todoAll, f)
+with open('todo_all_employees.json', mode='w') as f:
+    json.dump(todosAll, f)
