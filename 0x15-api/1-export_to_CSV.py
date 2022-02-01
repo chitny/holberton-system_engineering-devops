ade8cabc0c5ed3bbfbf89283115684e000fb7a25
Ignacio Chitnisky <elinmortal@gmail.com>
2022-02-01 00:49:23 +0000

    trying with 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module: nombre
+Descripcion del modulo
+
+"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    userId = sys.argv[1]
+    userName = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(userId))
+
+    name = userName.json().get('username')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTodo = 0
+    doneTodo = 0
+
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])
